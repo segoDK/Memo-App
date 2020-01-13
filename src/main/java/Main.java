2e845981b1937dff0562251af613d2e5419e854c
Niko7965 <54095558+Niko7965@users.noreply.github.com>
2020-01-13 11:23:45 +0100

    #gettingStarted

---
+++ src/main/java/Main.java
@@ -0,0 +1,34 @@
+import javafx.stage.Stage;
+import processing.core.PApplet;
+import processing.sound.*;
+
+
+
+public class Main extends PApplet {
+
+    public static void main(String[] args) {
+        // write your code here
+
+        PApplet.main("com.company.Main");
+        System.out.println("HelloWorld");
+        new Screen().start(args);
+
+    }
+    public void settings(){
+        //Put stuff like size() here
+        size(1500,800);
+
+
+    }
+    public void setup(){
+        //Ordinary setup
+        Sound s;
+        SinOsc sin = new SinOsc(this);
+        sin.play();
+        sin.freq(440);
+        getSurface().setVisible(false);
+    }
+    public void draw(){
+        rect(0, 0, 20, 20);
+    }
+}
